2feea0215a424d14431255f1e148e9d59559ffc1
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-03-12 19:07:12 +0100

    commit 4

--- PhoneConsoleApp-lab1/TestProjectPhone/UnitTest1.cs
+++ PhoneConsoleApp-lab1/TestProjectPhone/UnitTest1.cs
@@ -6,21 +6,43 @@ namespace TestProjectPhone
     [TestClass]
     public class UnitTest1
     {
+        //Test metody konstruktora z poprawnymi danymi wejœciowymi
         [TestMethod]
         public void Test_Konstruktor_Dane_poprawne()
         {
             //AAA
 
-            //Arrange
+            //Arrange - przygotowanie danych testowych
             var wlasciciel = "Molenda";
             var numerTelefonu = "123456789";
 
-            //Act 
+            //Act - wykonanie testowanego kodu 
             var phone = new Phone(wlasciciel, numerTelefonu);
 
-            //Assert 
+            //Assert - sprawdzenie poprawnoœci dzia³ania testowanego kodu
             Assert.AreEqual(wlasciciel, phone.Owner);
             Assert.AreEqual(numerTelefonu, phone.PhoneNumber);
+
         }
+
+        //Test metody konstruktora z niepoprawnymi danymi wejœciowymi
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_Konstruktor_EmptyOwner()
+        {
+            //Arange - przygotowanie danych testowych 
+            var owner = "";
+            var phoneNumber = "123456789";
+
+            //Act - wykonanie testowanego kodu
+            var phone = new Phone(owner, phoneNumber);
+
+            //Assert - sprawdzenie poprawnoœci dzia³ania testowanego kodu
+
+            //Wymagane jest rzucenie wyj¹tku ArgumentException, który mamy oznaczone w ExpectedException powy¿ej
+
+        }
+
+
     }
 }
