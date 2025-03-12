3ab7ecbd43b1e7278a818cfc896b85cebb43e93a
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-03-12 20:00:33 +0100

    commit 6

--- PhoneConsoleApp-lab1/TestProjectPhone/UnitTest1.cs
+++ PhoneConsoleApp-lab1/TestProjectPhone/UnitTest1.cs
@@ -8,50 +8,67 @@ namespace TestProjectPhone
     public class UnitTest1
     {
         //Test metody konstruktora z poprawnymi danymi wejœciowymi
+
+        //Atrybut oznaczaj¹cy metodê jako testow¹
         [TestMethod]
+
+        //Metoda testowa sprawdzaj¹ca poprawnoœæ dzia³ania konstruktora
         public void Test_Konstruktor_Dane_poprawne()
         {
             //AAA
 
             //Arrange - przygotowanie danych testowych
-            var wlasciciel = "Bacior";
-            var numerTelefonu = "123456789";
+            var wlasciciel = "Bacior";                              //przypadek kiedy w³aœcicielem telefonu jest Bacior
+            var numerTelefonu = "123456789";                        //przypadek kiedy numer telefonu jest poprawny
 
             //Act - wykonanie testowanego kodu 
-            var phone = new Phone(wlasciciel, numerTelefonu);
+            var phone = new Phone(wlasciciel, numerTelefonu);       //utworzenie obiektu klasy Phone z³ozo¿onej z w³aœciciela i numeru telefonu 
 
-            //Assert - sprawdzenie poprawnoœci dzia³ania testowanego kodu
-            Assert.AreEqual(wlasciciel, phone.Owner);
-            Assert.AreEqual(numerTelefonu, phone.PhoneNumber);
-
+            //Assert - sprawdzenie poprawnoœci dzia³ania testowanego kodu 
+            
+            Assert.AreEqual(wlasciciel, phone.Owner);               //sprawdzenie czy w³aœcicielem telefonu jest Bacior
+            Assert.AreEqual(numerTelefonu, phone.PhoneNumber);      //sprawdzenie czy numer telefonu jest poprawny
         }
 
+
         //Test metody konstruktora z niepoprawnymi danymi wejœciowymi
+        //Atrybut oznaczaj¹cy metodê jako testow¹
         [TestMethod]
+
+        //Atrybut oznaczaj¹cy, ¿e metoda powinna zakoñczyæ siê wyj¹tkiem ArgumentException
         [ExpectedException(typeof(ArgumentException))]
+
+        //Metoda testowa sprawdzaj¹ca poprawnoœæ dzia³ania konstruktora
         public void Test_Konstruktor_Pusty_Wlascicel()
         {
             //Arange - przygotowanie danych testowych 
-            var wlasciciel = "";
-            var numerTelefonu = "123456789";
+            var wlasciciel = "";                                    //przypadek kiedy w³aœcicielem telefonu jest pusty
+            var numerTelefonu = "123456789";                        //przypadek kiedy numer telefonu jest poprawny
 
             //Act - wykonanie testowanego kodu
-            var phone = new Phone(wlasciciel, numerTelefonu);
+            var phone = new Phone(wlasciciel, numerTelefonu);       //utworzenie obiektu klasy Phone z w³aœcicielem i numerem telefonu
 
             //Assert - sprawdzenie poprawnoœci dzia³ania testowanego kodu
-            //Wymagane jest rzucenie wyj¹tku ArgumentException, który mamy oznaczony w ExpectedException powy¿ej, b³¹d wyskoczy gdy w³aœciciel bêdzie pusty
+            /* Wymagane jest rzucenie wyj¹tku ArgumentException, który mamy oznaczony
+            w ExpectedException powy¿ej, b³¹d wyskoczy gdy w³aœciciel bêdzie pusty */
         }
 
+
+        //Test metody konstruktora z niepoprawnymi danymi wejœciowymi
         [TestMethod]
+
+        //Atrybut oznaczaj¹cy, ¿e metoda powinna zakoñczyæ siê wyj¹tkiem ArgumentException
         [ExpectedException(typeof(ArgumentException))]
+
+        //Metoda testowa sprawdzaj¹ca poprawnoœæ dzia³ania konstruktora
         public void Test_Konstruktor_Niepoprawny_Numer()
         {
             //Arange - przygotowanie danych testowych
-            var wlasciciel = "Bacior";
-            var numerTelefonu = "1234"; //przypadek kiedy to podany numer telefonu jest za krótki (mniejszy ni¿ 9 cyfr)
+            var wlasciciel = "Bacior";                              //przypadek kiedy w³aœcicielem telefonu jest Bacior
+            var numerTelefonu = "1234";                             //przypadek kiedy to podany numer telefonu jest za krótki (mniejszy ni¿ 9 cyfr)
 
             //Act - wykonanie testowanego kodu 
-            var phone = new Phone(wlasciciel, numerTelefonu);
+            var phone = new Phone(wlasciciel, numerTelefonu);       //utworzenie obiektu klasy Phone z w³aœcicielem i numerem telefonu
 
             //Assert - sprawdzenie poprawnoœci dzia³ania testowanego kodu
             //Wymagane jest rzucenie wyj¹tku ArgumentException, który mamy oznaczony w ExpectedException powy¿ej - b³¹d wyskoczy, gdy numer bêdzie zbyt krótki 
